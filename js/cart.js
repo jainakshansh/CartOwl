e06127fbf12194e35cf63540b209d7b80b471e65
Akshansh Jain <akshanshjn9@gmail.com>
2018-03-08 22:42:18 +0530

    Shopping Cart using AngularJS

---
+++ js/cart.js
@@ -0,0 +1,28 @@
+var myApp = angular.module("ShopCart", []);
+
+myApp.controller("cartCont", function($scope) {
+
+	$scope.price1 = 0;
+	$scope.price2 = 0;
+	$scope.price3 = 0;
+
+	$scope.res1 = function() {
+		$scope.price1 = $scope.onequant * 13999;
+		return $scope.price1;
+	}
+
+	$scope.res2 = function() {
+		$scope.price2 = $scope.twoquant * 14750;
+		return $scope.price2;
+	}
+
+	$scope.res3 = function() {
+		$scope.price3 = $scope.threequant * 13999;
+		return $scope.price3;
+	}
+
+	$scope.gtotal = function() {
+		return $scope.price1 + $scope.price2 + $scope.price3;
+	}
+
+});
